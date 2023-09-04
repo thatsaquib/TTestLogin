37fb4549640e11f10c75f6e280d0bb04634ce329
verr-nice <verr-nice@DESKTOP-1HM2017>
2023-09-04 21:15:34 +0530

    Add project files.

---
+++ TTestLogin/Controllers/StartController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TTestLogin.Controllers
+{
+    public class StartController : Controller
+    {
+        // GET: Start
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string username, string password)
+        {
+            ViewBag.User = username;
+            return View();
+        }
+    }
+}
